0892ca797d2d05a86c80ad7196a1facba34a0c88
kylejava <kyleedwardgjava@gmail.com>
2020-12-04 14:53:38 -0800

    Created Method to parse through users tweet to get birthday

--- functions.py
+++ functions.py
@@ -8,4 +8,34 @@ def getBirthday(bithday):
     elif x != 0:
         return x
 
-print("test")
+
+def return_birthday(queue):
+    current_num = ""
+    num_arr = []
+    for i in range(len(queue)):
+        if(queue[i].isdigit()):
+            current_num += queue[i]
+        else:
+            num_arr.append(current_num)
+            current_num = ""
+    num_arr.append(current_num)
+    return num_arr
+
+
+def obtain_date(s):
+    num_arr = []
+    queue = []
+    birthday = 0
+    for i in range(len(s)):
+
+        if(s[i].isdigit() or s[i] == '/'):
+            queue.append(s[i])
+
+
+    num_arr = return_birthday(queue)
+
+    for i in range(len(num_arr)):
+        num = int(num_arr[i])
+        birthday += num
+
+    return birthday
---
+++ main.py
@@ -0,0 +1,10 @@
+import keys as s
+import tweepy
+import json
+import requests
+from functions import *
+
+
+auth = tweepy.OAuthHandler(s.consumer_key, s.consumer_secret)
+auth.set_access_token(s.access_token, s.access_token_secret)
+api = tweepy.API(auth)
